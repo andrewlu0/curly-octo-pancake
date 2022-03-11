71c954add344beb84fa13259bc2e4a0fb85f7725
Andrew Luo <32521085+andrewlu0@users.noreply.github.com>
2022-03-11 13:29:29 -0800

    module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/andrewlu0/curly-octo-pancake/curto/curto
+
+go 1.17
--- server/server.go
+++ server/server.go
@@ -27,7 +27,7 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	pb "github.com/andrewlu0/curly-octo-pancake/curto/curto"
 )
 
 var (
